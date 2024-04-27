fc363af6c3550e4cfbfc14f98711b9d9f210c76b
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2024-04-27 06:53:31 +0100

    change todo's from static html to dynamically rendered html

---
+++ js/data.js
@@ -0,0 +1,24 @@
+const todoData = [
+  {
+    description: `jog around the park 3x`, 
+    uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee'
+  }, 
+  {
+    description: `10 minutes meditation`, 
+    uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2'
+  }, 
+  {
+    description: `Read for 1 hour`, 
+    uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45'
+  }, 
+  {
+    description: `Pick up groceries`, 
+    uuid: '7l289slb-c0f5-4545-9c4b-77835tgs2'
+  }, 
+  {
+    description: `Complete todo app on frontend mentor`, 
+    uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45'
+  }
+]
+
+export { todoData }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+import { todoData } from './data.js'
 
+const themeToggleBtn = document.querySelector('.theme-toggle-btn')
+const todoItemsDiv = document.getElementById('todo-items')
+
+// Toggle Theme 
 themeToggleBtn.addEventListener('click', function() {
-  document.body.classList.toggle('light-theme');
+  document.body.classList.toggle('light-theme')
   
   if (document.body.classList.contains('light-theme')) {
     themeToggleBtn.src = './images/icon-moon.svg'
@@ -9,3 +13,39 @@ themeToggleBtn.addEventListener('click', function() {
     themeToggleBtn.src = './images/icon-sun.svg'
   }
 })
+
+
+
+// Get todo items from data, create html for it. 
+function getTodoItems() {
+  let todoItems = ``
+  todoItems += `
+  <div class="todo-item">
+    <div class="circle todo-item-checkmark show-check"> <!-- class: 'show-check' --> 
+      <img class="checkmark" src="./images/icon-check.svg">
+    </div>
+    <p class="todo-item-description checked">jog around the park 3x</p> <!-- class: 'checked' --> 
+    <img class="todo-item-delete" src="./images/icon-cross.svg">
+  </div>
+  `
+  todoData.forEach(todo => {
+    todoItems += `
+    <div class="todo-item">
+      <div class="circle todo-item-checkmark">
+        <img class="checkmark" src="./images/icon-check.svg">
+      </div>
+      <p class="todo-item-description">${todo.description}</p>
+      <img class="todo-item-delete" src="./images/icon-cross.svg">
+    </div>
+    `
+  })
+
+  return todoItems
+}
+
+// Render todo items
+function render() {
+  todoItemsDiv.innerHTML = getTodoItems()
+}
+
+render()
