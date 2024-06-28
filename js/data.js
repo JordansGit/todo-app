ebc0732e327dd22b5855d79938cebcb06d2038e1
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2024-06-28 20:48:55 +0100

    feat: add new todo

--- js/data.js
+++ js/data.js
@@ -2,27 +2,27 @@ const todoData = [
   {
     description: `jog around the park 3x`, 
     uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee', 
-    checked: false
+    isChecked: false
   }, 
   {
     description: `10 minutes meditation`, 
     uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
-    checked: false
+    isChecked: false
   }, 
   {
     description: `Read for 1 hour`, 
     uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45', 
-    checked: false
+    isChecked: false
   }, 
   {
     description: `Pick up groceries`, 
     uuid: '7l289slb-c0f5-4545-9c4b-77835tgs2', 
-    checked: false
+    isChecked: false
   }, 
   {
     description: `Complete todo app on frontend mentor`, 
     uuid: '8sf341hdd-c0f5-4545-9c4b-1237gyys45', 
-    checked: false
+    isChecked: false
   }
 ]
 
--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 import { todoData } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 const themeToggleBtn = document.querySelector('.theme-toggle-btn')
-const todoItemsDiv = document.getElementById('todo-items')
 const todoListDiv = document.getElementById('todo-list')
+const todoSubmit = document.getElementById('todo-submit')
 
 // Toggle Theme 
 themeToggleBtn.addEventListener('click', function() {
@@ -17,46 +18,64 @@ themeToggleBtn.addEventListener('click', function() {
 
 render() /* need to call render first so the html of the todo's loads. */
 
- 
+
+// Submit new todo's
+todoSubmit.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  let newTodoEl = document.getElementById('todo-new-input')
+  // when user submits form (via enter), do x action 
+  const newTodo = {
+    description: newTodoEl.value, 
+    uuid: uuidv4(), 
+    isChecked: false
+  }
+
+  todoData.unshift(newTodo)
+  render()
+  newTodoEl.value = ''
+}
+
+
 
 // Delete Todo's & Toggle marked/checked for todos 
 todoListDiv.addEventListener('click', function(e) {
   if (e.target.dataset.todoItem) { /* if anywhere inside todo div is clicked, except del btn, do ... */ 
-    // mark the checked todo. 
-    toggleTodoChecked(e.target)
+    toggleTodoChecked(e.target.dataset.todoItem) 
   } else if (e.target.dataset.todoDel) {  /* if delete btn is selected, do ... */ 
-    // delete todo 
     deleteTodo(e.target.dataset.todoDel)
   }  
 })
 
-function toggleTodoChecked(todoItem) {
-  const targetTweetObj = todoData.filter(todo => todo.uuid === todoItem.dataset.todoItem)[0]
+function toggleTodoChecked(todoItemId) {
+  const targetTweetObj = todoData.filter(todo => todo.uuid === todoItemId)[0]
 
-  targetTweetObj.checked = !targetTweetObj.checked
+  targetTweetObj.isChecked = !targetTweetObj.isChecked
   render()
 }
 
 function deleteTodo(todoItemId) {
   const targetTweetObj = todoData.filter(todo => todo.uuid === todoItemId)[0] /* get target tweet obj */ 
   const index = todoData.indexOf(targetTweetObj)
-  //todoData.forEach(todo => console.log(todo === targetTweetObj))
+
   todoData.splice(index, 1)
   render()
-  console.log(todoData)
 }
+
+
 
 // Dynamically Render Todo's
 function getTodoItems() {
   let todoItems = ``
 
-  /* this is dummy text for now to show what happens when I have a certain todos checked. */ 
   if (todoData.length > 0) {
     todoData.forEach(todo => {
       let checkmarkClass = ''
       let checkedDescriptionClass = ''
     
-      if (todo.checked) {
+      if (todo.isChecked) {
         checkmarkClass = 'show-check'
         checkedDescriptionClass = 'checked'
       }     
@@ -71,28 +90,34 @@ function getTodoItems() {
       </div>
       `
     })
-  
   }
 
   return todoItems
 }
 
 function render() {
+  const todoItemsDiv = document.getElementById('todo-items')
+
   todoItemsDiv.innerHTML = getTodoItems()
+  console.log(todoData)
 }
 
-render() // don't need to call this as it's already being called during toggle toggleTodoChecked()
+//render() // don't need to call this as it's already being called during toggle toggleTodoChecked()
 
 
 
-// currently working on adding toggleCheckedTodo functionality 
+/* Bugs
+  css: the top banner doesn't extend the image to cover entire width. it repeats itself. 
+  css: the design breaks once the list of todo's extends passed current screen. the bottom bg turns white, can't see the footer text. 
+
+*/ 
+
 
 /* Learning Notes 
 Delete Todo's Function
   for my delete todo's function, currently I'm using array.splice. 
   I'm thinking is there a better more appropriate, non-destructive way I should be doing this instead. 
   for now I'll leave it tho. Might come back and edit this function in the future. 
-
 
 
 */ 
